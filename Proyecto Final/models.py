7ff8ebca64dedb2b39672d1e1264d65c14145f25
Bryan11710 <ba.cordovao@uea.edu.ec>
2026-02-26 15:38:58 -0500

    Evolución de Semana 10 a Proyecto Final: Mejora de interfaz y lógica POO

---
+++ Proyecto Final/models.py
@@ -0,0 +1,7 @@
+class Celular:
+    def __init__(self, id, nombre, marca, cantidad, precio):
+        self.id = id
+        self.nombre = nombre
+        self.marca = marca
+        self.cantidad = cantidad
+        self.precio = precio
